766f89656f27c5711b01c90171893118cdc9ba5c
gustavobarbozamarques <gustavomarques.contato@gmail.com>
2022-05-20 20:38:54 -0300

    Swagger config and Security fixes

---
+++ src/main/java/br/com/gustavobarbozamarques/springbootsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.com.gustavobarbozamarques.springbootsecurityjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //Authorization should happen in controller @PreAuthorize annotations, not here.
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
+
+        //Disable session for a stateless Rest MicroService.
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+}
---
+++ src/main/java/br/com/gustavobarbozamarques/springbootsecurityjwt/controllers/ResourceController.java
@@ -0,0 +1,46 @@
+package br.com.gustavobarbozamarques.springbootsecurityjwt.controllers;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/resource")
+public class ResourceController {
+
+    @GetMapping("/public")
+    @ApiOperation("Anyone can access.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "JWT Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class)
+    })
+    public String publicResource() {
+        return "Public resource, no authentication needed.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("isAuthenticated()")
+    @ApiOperation("Only authenticated users can access")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "JWT Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class)
+    })
+    public String userResource(@ApiIgnore Principal principal) {
+        return "Welcome user, %s".formatted(principal.getName());
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("isAuthenticated() and hasRole('ADMIN')")
+    @ApiOperation("Only authenticated users with ADMIN role can access")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "JWT Access Token", required = true, allowEmptyValue = false, paramType = "header", dataTypeClass = String.class)
+    })
+    public String adminResource(@ApiIgnore Principal principal) {
+        return "Welcome admin, %s".formatted(principal.getName());
+    }
+}
